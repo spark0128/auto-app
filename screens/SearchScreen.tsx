2d898a451d5bc5a28aa5f406a15e02223c903c1f
blaswan <lsw9549@gmail.com>
2019-10-09 16:45:54 +0900

    Add sub components in Search Tab

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,14 +1,53 @@
-import React from 'react';
-import {
-  Text,
-} from 'react-native';
+import React, { useState } from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
+import { SearchBar } from 'react-native-elements';
+
+import { Button, ListItem } from 'react-native-elements';
+import { ListHeader } from '../components/Search/ListHeader';
+
 
 export default function SearchScreen() {
+
+  // TODO: Get popular brands from server
+  const popularBrands = [
+    { id: '1', name: 'Toyota' },
+    { id: '2', name: 'Lexus' },
+  ];
+
+  // TODO: Get other brands from server
+  const otherBrands = [
+    { id: '1', name: 'Toyota' },
+    { id: '2', name: 'Lexus' },
+  ];
+
+  const [search, setSearch] = useState('');
+
   return (
-    <ScrollView style={styles.container}>
-      <Text>Search</Text>
-    </ScrollView>
+    <>
+      <SearchBar
+        placeholder="Search a car here."
+        onChangeText={(search) => setSearch(search)}
+        value={search} />
+      <ScrollView style={styles.container}>
+        <ListHeader title='Popular brands' />
+        {popularBrands.map((brand) => {
+          return <ListItem
+            key={brand.id}
+            title={brand.name}
+            bottomDivider />;
+        })}
+        <ListHeader title='Other brands' />
+        {otherBrands.map((brand) => {
+          return <ListItem
+            key={brand.id}
+            title={brand.name}
+            bottomDivider />;
+        })}
+      </ScrollView>
+      <Button
+        style={styles.searchButton}
+        title="Search (95,108 Cars)" />
+    </>
   );
 }
 
@@ -19,7 +58,13 @@ SearchScreen.navigationOptions = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: '#f5f5f5',
+  },
+  listHeader: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+  },
+  searchButton: {
+    height: 46,
   },
 });
