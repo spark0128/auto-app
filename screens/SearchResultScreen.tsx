e4a936e850fbfe91281c25d31877da66d65f51c0
blaswan <lsw9549@gmail.com>
2019-10-09 17:34:43 +0900

    Add FloatingButton in SearchResultScreen

--- screens/SearchResultScreen.tsx
+++ screens/SearchResultScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
+import { ButtonGroup } from 'react-native-elements';
 import { FeaturedCarCard } from '../components/Home/FeaturedCarCard';
 
 export default function SearchResultScreen() {
@@ -9,12 +10,27 @@ export default function SearchResultScreen() {
     { id: '1', name: 'LEXUS LX300' },
   ];
 
+  // Handlers
+  const onFloatingButtonClick = (index) => {
+    if (index === 0) {
+      console.log("filter button clicked");
+    } else if (index === 1) {
+      console.log("order button clicked");
+    }
+  }
+
   return (
-    <ScrollView style={styles.container}>
-      {cars.map((car) => {
-        return <FeaturedCarCard key={car.id} car={car} />
-      })}
-    </ScrollView>
+    <>
+      <ScrollView style={styles.container}>
+        {cars.map((car) => {
+          return <FeaturedCarCard key={car.id} car={car} />
+        })}
+      </ScrollView>
+      <ButtonGroup
+        containerStyle={styles.floatingButton}
+        onPress={onFloatingButtonClick}
+        buttons={['Filter', 'Order']} />
+    </>
   );
 }
 
@@ -28,5 +44,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f5f5f5',
+  },
+  floatingButton: {
+    position: 'absolute',
+    width: 148,
+    height: 30,
+    bottom: 10,
+    alignSelf: 'center'
   },
 });
